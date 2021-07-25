f541ef152a764868f70cf6209e08f3c815547b56
Pooja J. Kamble <42534109+PoojaJKamble@users.noreply.github.com>
2021-07-25 19:10:48 +0530

    Added Comments

--- RollingDice.py
+++ RollingDice.py
@@ -1,18 +1,18 @@
-import random
-from tkinter import *
+import random   #Select ramdom choice
+from tkinter import *   #import all methods from tkinter(GUI library)
 
-root = Tk()
-root.geometry("700x450")
+root = Tk()     #initialize tkinter
+root.geometry("700x450")    #page size
 
-l1 = Label(root, font =( 'times',200))
+l1 = Label(root, font =( 'times',200))   #widget that is used to implement display boxes where you can place
 
 def roll():
     print("Rolling Dice")
     print("\U0001F600")
-    number = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
-    l1.config(text = f'{random.choice(number)}{random.choice(number)}')
+    number = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']    #Display dots on Dice
+    l1.config(text = f'{random.choice(number)}{random.choice(number)}')     #Select random choice on rolling Dice
     l1.pack()
-b1 = Button(root, text ="Let's Roll Dice",width = 15 ,command = roll)
+b1 = Button(root, text ="Let's Roll Dice",width = 15 ,command = roll)       #Button to roll Dice
 b1.place(x =300 , y= 0 )
 
 root.mainloop()
